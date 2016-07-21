c2861727ab348a71ad17efa41a1bd3cfdf5437c1
MohamedDiaa <mohameddiaa1988@hotmail.com>
2016-07-21 17:34:36 +0200

    added missing colon in config file

--- config/database.js
+++ config/database.js
@@ -1,5 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://Diaa:DB123@ec2-52-24-169-69.us-west-2.compute.amazonaws.com:27017/meanstacktutorials'
-	//'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    remoteUrl : 'mongodb://Diaa:DB123@ec2-52-24-169-69.us-west-2.compute.amazonaws.com:27017/meanstacktutorials',
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
