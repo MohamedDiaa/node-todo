68650bae1b2073e499464a255ef4df81b4873222
MohamedDiaa <mohameddiaa1988@hotmail.com>
2016-07-21 18:02:33 +0200

    testing configurations

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://Diaa:DB123@ec2-52-24-169-69.us-west-2.compute.amazonaws.com:27017/meanstacktutorials',
+    remoteUrl : 'mongodb://Diaa:DB123@ec2-52-24-169-69.us-west-2.compute.amazonaws.com:27017/dummyDB',
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
