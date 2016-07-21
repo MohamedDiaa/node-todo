8bddf2a711ce70d4547596206789f3fd1c37dc25
MohamedDiaa <mohameddiaa1988@hotmail.com>
2016-07-21 17:29:00 +0200

    updated server URL in config file

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    remoteUrl : 'mongodb://Diaa:DB123@ec2-52-24-169-69.us-west-2.compute.amazonaws.com:27017/meanstacktutorials'
+	//'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
